8604b136f0727a11d0108e9fe2d420aecf2ec75a
Nicholas Evans <ppertinate@gmail.com>
2023-09-19 01:22:41 -0500

    disabled vite screen clear

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React test</h1>
       <div className="card">
         <button
           onClick={() => {
