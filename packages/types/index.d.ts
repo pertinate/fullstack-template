847613a636bfa649b6fd285e07be881ddee6dd86
Nicholas Evans <ppertinate@gmail.com>
2023-09-19 01:12:47 -0500

    added zod to types

--- packages/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type Test = string;
-
-export type * from "@prisma/client";
---
+++ packages/types/index.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export type Test = string;
+
+export type * from "@prisma/client";
+
+export const TesttSchema = z.object({
+  str: z.string(),
+  nested: z.object({
+    val: z.number(),
+  }),
+});
+
+export type Testt = z.infer<typeof TesttSchema>;
