129e445109cb13aa966c67a334f86add4c38f91f
Nicholas Evans <ppertinate@gmail.com>
2023-09-20 19:19:30 -0500

    added shared routing between backend and frontend beginning implementation

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,47 +1,62 @@
-import { testGet, testPost } from "./controllers/controllerTest";
-import Fastify from "fastify";
-import fastifyStatic from "@fastify/static";
-import fs from "fs";
-import path from "path";
+import { testGet, testPost } from './controllers/controllerTest'
+import Fastify from 'fastify'
+import fastifyStatic from '@fastify/static'
+import fs from 'fs'
+import path from 'path'
+import { Metadata, routingFactory } from '@local/types'
 
-const fastify = Fastify({ logger: true });
+const defaultMeta: Metadata = {}
+
+export const routes = routingFactory({
+    '/': () => defaultMeta,
+    '/about': () => defaultMeta,
+    '/about/me': () => defaultMeta,
+    '/about/product_number_one': () => defaultMeta,
+    '/about/:productId': (path, queryString) => {
+        return {}
+    },
+})
+
+const fastify = Fastify({ logger: true })
 
 fastify.register(fastifyStatic, {
-  root: path.resolve("./../client/dist"),
-  prefix: "/public", // optional: default '/'
-  constraints: {},
-});
+    root: path.resolve('./../client/dist'),
+    prefix: '/public', // optional: default '/'
+    constraints: {},
+})
 
 fastify.setErrorHandler(function (error, request, reply) {
-  if (error instanceof Fastify.errorCodes.FST_ERR_BAD_STATUS_CODE) {
-    // Log error
-    // this.log.error(error);
-    // Send error response
-  } else {
-    // fastify will use parent error handler to handle this
-  }
-  reply.status(500).send({ ok: false });
-});
+    if (error instanceof Fastify.errorCodes.FST_ERR_BAD_STATUS_CODE) {
+        // Log error
+        // this.log.error(error);
+        // Send error response
+    } else {
+        // fastify will use parent error handler to handle this
+    }
+    reply.status(500).send({ ok: false })
+})
 
 fastify.register(
-  (fastify, opts, done) => {
-    fastify.get("/", testGet);
-    fastify.post("/", testPost);
-    done();
-  },
-  { prefix: "/api" }
-);
+    (fastify, opts, done) => {
+        fastify.get('/', testGet)
+        fastify.post('/', testPost)
+        done()
+    },
+    { prefix: '/api' },
+)
 
-fastify.get("/", (req, res) => {
-  console.log(path.resolve("./../client/dist/index.html"));
-  res.header("Content-Type", "text/html");
-  return fs.readFileSync(path.resolve("./../client/dist/index.html"));
-});
+fastify.get('/', (req, res) => {
+    console.log(path.resolve('./../client/dist/index.html'))
+    res.header('Content-Type', 'text/html')
+    return fs.readFileSync(path.resolve('./../client/dist/index.html'))
+})
 
 // Declare a route
 
 // Run the server!
-fastify.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
-  if (err) throw err;
-  // Server is now listening on ${address}
-});
+fastify.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
+    if (err) throw err
+    // Server is now listening on ${address}
+})
+
+console.log('>>>', routes.routes?.[0])
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -12,3 +12,47 @@ export const TesttSchema = z.object({
 })
 
 export type Testt = z.infer<typeof TesttSchema>
+
+export const MetadataSchema = z.object({})
+export type Metadata = z.infer<typeof MetadataSchema>
+
+type RouteCallback = (path: string, queryString: string) => Metadata
+
+export type Route<T extends string> = {
+    path: T
+    routes?: Route<T>[]
+    metadata?: RouteCallback
+}
+
+export const routingFactory = <T extends string>(
+    getMetaHash: Record<T, RouteCallback | undefined>,
+) => {
+    const routing: Route<T> = {
+        metadata: getMetaHash['/' as keyof typeof getMetaHash], //localhost/
+        path: '/' as keyof typeof getMetaHash,
+        routes: [
+            {
+                metadata: getMetaHash['/about' as keyof typeof getMetaHash], //localhost/about
+                path: '/about' as keyof typeof getMetaHash,
+                routes: [
+                    {
+                        metadata:
+                            getMetaHash[
+                                '/about/me' as keyof typeof getMetaHash
+                            ], //localhost/about/me
+                        path: '/me' as keyof typeof getMetaHash,
+                    },
+                    {
+                        metadata:
+                            getMetaHash[
+                                '/about/product_number_one' as keyof typeof getMetaHash
+                            ],
+                        path: '/product_number_one' as keyof typeof getMetaHash,
+                    },
+                ],
+            },
+        ],
+    }
+
+    return routing
+}
